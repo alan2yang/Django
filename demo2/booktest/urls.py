31fce763494ca865432aab63cc36c4d700123b5c
yf <yf_web@163.com>
2018-08-24 22:46:39 +0800

    form表单的使用

--- demo2/booktest/urls.py
+++ demo2/booktest/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r'^booktest/$',views.IndexView.as_view())
+    url(r'^booktest/$',views.IndexView.as_view()),
+    url(r'^book/$',views.BookView.as_view())
 ]
--- demo2/booktest/views.py
+++ demo2/booktest/views.py
@@ -1,11 +1,37 @@
 from urllib import request
 
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import View
+from .forms import BookForm
 
 
 class IndexView(View):
     def get(self,request):
-        return render(request,'index.html',{'city':'北京'})
+        context={
+            'city': '北京',
+            'alist':[1,2,3],
+            'adict':{
+                'name':'西游记',
+                'author':'吴承恩'
+            }
+        }
+        return render(request,'index.html',context)
+
+
+class BookView(View):
+    def get(self,request):
+        form=BookForm()
+        return render(request,'index.html',{'form':form})
+
+    def post(self,request):
+        form=BookForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponse('ok')
+
+        else:
+            return render(request,'index.html',{'form':form})
+
